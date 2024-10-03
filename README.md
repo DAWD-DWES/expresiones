0f1fe5a2524debead2b7bc45c5da6a6005aa69ab
jivanrod <jivanrod@gmail.com>
2024-10-03 18:16:06 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # expresiones
-Desarrolla las expresiones que se plantean en los enunciados de la página web. Busca en el código
+Desarrolla las expresiones que se plantean en los párrafos de la página web. Busca en el código
 las ubicaciones dónde incluir el código en el script PHP, dichas ubicaciones se marcan con el comentario HTML "Escribe tu código aquí".
 La solución propuesta del ejercicio se encuentra en la rama "solución"
--- index.php
+++ index.php
@@ -9,13 +9,16 @@
     <body>
         <h2>Programación con expresiones</h2>
         <table>
-            <tbody>
-                <tr><td colspan="3">
-                        <p><b>Obtención de una nota simbólica a partir de una nota numérica según los rangos: >=90 A, >=80 B, >=70 C, >=60 D, <60 F</b></p>
-                        <p>Mostrar el mensaje: La nota corresponde a B</p></td></tr>
-                <tr>
-                    <td>$notaNumerica = 85;</td>
-                    <td class="respuesta"><pre>match (true) {
+            <tr><td colspan="3"><p>Cálculo del área de un círculo</p></td></tr>
+            <tr>
+                <td>$radio = 563;</td>
+                <td class="respuesta">pi() * $radio ** 2;</td>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr><td colspan="3"><p>Obtención de una nota simbólica a partir de una nota numérica según los rangos: >=90 A, >=80 B, >=70 C, >=60 D, <60 F</p></td></tr>
+            <tr>
+                <td>$notaNumerica = 85;</td>
+                <td class="respuesta"><pre>match (true) {
     $notaNumerica >= 90 => 'A',
     $notaNumerica >= 80 => 'B',
     $notaNumerica >= 70 => 'C',
@@ -23,56 +26,38 @@
     $notaNumerica < 60 => 'F',
     default => 'Nota inválida'
 }</pre></td>
-                    <td><!-- Escribe tu código aquí --></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr><td colspan="3"><p><b>Cálculo del precio final después de aplicar un descuento</b></p>
-                        <p>Mostrar el mensaje: El precio final es 478.55</p></td></tr>
-                <tr>
-                    <td><pre>$precio = 563;
-$descuento = 15;</pre></td>
-                    <td class="respuesta">$precio * (1 - $descuento / 100);</td>
-                    <td><!-- Escribe tu código aquí --></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td colspan = "3"><p><b>Determina si un año es bisiesto</b></p>
-                        <p>Mostrar el mensaje: El año 2024 es bisiesto</p></td>
-                </tr>
-                <tr>
-                    <td>$anio = 2024;</td>
-                    <td class="respuesta">($anio % 4 == 0 && $anio % 100 != 0) || ($anio % 400 == 0)</td>
-                    <td><!-- Escribe tu código aquí --></td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td colspan = "3"><p><b>Conversión de Celsius a Farenheit</b></p>
-                        <p>Mostrar el mensaje: 25 grados Celsius corresponden a 77 grados Farenheit</p></td></tr>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr><td colspan="3"><p>Cálculo del precio final después de aplicar un descuento</p></td></tr>
+            <tr>
+                <td>$precio = 563; $descuento = 15;</td>
+                <td class="respuesta">$precio * (1 - $descuento / 100);</td>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td colspan = "3"><p>Determina si un año es bisiesto</p></td>
+            </tr>
+            <tr>
+                <td>$anio = 2024;</td>
+                <td class="respuesta">($anio % 4 == 0 && $anio % 100 != 0) || ($anio % 400 == 0)</td>
+                <td><!-- Escribe tu código aquí --></td>
+            </tr>
+            <tr>
+                <td colspan = "3"><p>Conversión de Celsius a Farenheit</p></td></tr>
             <td>$celsius = 25;</td>
             <td class="respuesta">($celsius * 9/5) + 32</td>
             <td><!-- Escribe tu código aquí --></td>
         </tr>
-    </tbody>
-    <tbody>
         <tr>
-            <td colspan = "3"><p><b>Obtener las iniciales de un nombre y apellidos</b></p>
-                <p>Mostrar el mensaje: Las iniciales de Juan Perez Martinez son J P M</p></td>
+            <td colspan = "3"><p>Obtener las iniciales de un nombre y apellidos</p></td>
         </tr>
         <tr>
             <td>$nombre = 'Juan Perez Martinez';</td>
             <td class="respuesta">str_replace(range('a', 'z'), '', $nombre)</td>
             <td><!-- Escribe tu código aquí --></td>
         </tr>
-    </tbody>
-    <tbody>
-        <tr><td colspan = "3"><p><b>Comprueba si una fecha es correcta entre el año 1000 y 3000</b></p>
-                <p>Mostrar el mensaje: La fecha 28/2/1399 es correcta</p></td><tr>
-        <tr><td><pre>$dia = 28;
-$mes = 2;
-$anio = 1399;</pre></td>
+        <tr><td colspan = "3"><p>Comprueba si una fecha es correcta entre el año 1000 y 3000</p></td><tr>
+        <tr><td>$dia = 28; $mes = 2; $anio = 1399;</td>
             <td class="respuesta"><pre>($anyo > 1000) && ($anyo < 3000) &&
                 (match ($mes) {
                     4, 6, 9, 11 => ($dia >= 1) && ($dia <= 30),
@@ -83,10 +68,8 @@ $anio = 1399;</pre></td>
                 })</pre></td>
             <td><!-- Escribe tu código aquí --></td>
         </tr>
-    <tbody>
         <tr>
-            <td colspan = "3"><p><b>Determinar si un DNI es correcto</b></p>
-                <p>Mostrar el mensaje: El DNI 44926413G es Incorrecto</p></td>
+            <td colspan = "3"><p>Determinar si un DNI es correcto</p></td>
         </tr>
         <tr>
             <td>$DNI = '44926413G';</td>
@@ -95,8 +78,7 @@ $anio = 1399;</pre></td>
             <td><!-- Escribe tu código aquí --></td>
         </tr>
         <tr>
-            <td colspan = "3"><p><b>Comprobar si alguien es mayor de edad con su fecha de nacimiento</b></p>
-                <p>Mostrar el mensaje: Es mayor de edad</p></td>
+            <td colspan = "3"><p>Comprobar si alguien es mayor de edad con su fecha de nacimiento</p></td>
         </tr>
         <tr>
             <td><pre>$fecha = '13/05/2001';
@@ -105,9 +87,6 @@ $fechaHoy = '23/10/2023';</pre></td>
                 (substr($fechaHoy, 3, 2) < substr($fechaNac, 3, 2) || substr($fechaHoy, 3, 2) == substr($fechaNac, 3, 2) && substr($fechaHoy, 0, 2) < substr($fechaNac, 0, 2))) > 18</td>
             <td><!-- Escribe tu código aquí --></td>
         </tr>
-    </tbody>
-</table>
+    </table>
 </body>
 </html>
-
-
